3980828721e35a6e61b3683200dab2082f43316a
Benjamin Espinoza <beespinoza2022@inf.udec.cl>
2024-06-03 21:09:16 -0400

    se cambia la manera en la que se muestran los graficos

--- grafic_ids.py
+++ grafic_ids.py
@@ -11,33 +11,64 @@ df['duration'] = pd.to_numeric(df['duration'])
 avg_df = df.groupby(['hash_type', 'operation', 'n']).mean().reset_index()
 
 # Crear subgráficas para inserciones y búsquedas
-fig, ax = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
+fig, ax = plt.subplots(1, 1, figsize=(12, 10), sharex=True)
 
 # Graficar los tiempos de inserción promedio
 insert_df = avg_df[avg_df['operation'].str.contains('insert')]
 for key, grp in insert_df.groupby(['hash_type', 'operation']):
-    ax[0].plot(grp['n'], grp['duration'], marker='o', linestyle='-', label=f'{key[0]} - {key[1]}')
+    ax.plot(grp['n'], grp['duration'], marker='o', linestyle='-', label=f'{key}')
 
-ax[0].set_title('Tiempos de Inserción Promedio')
-ax[0].set_ylabel('Duración Promedio (ns)')
-ax[0].set_yscale('log')
-#ax[0].set_xscale('log')
-ax[0].legend()
+ax.set_title('Tiempos de Inserción Promedio', fontsize = 16, fontweight = 'bold')
+ax.set_ylabel('Duración Promedio (ns)', fontsize = 14, fontweight = 'bold')
+#ax.set_yscale('log')
+#ax.set_xscale('log')
+ax.legend()
 
+plt.xticks(rotation=45)
+plt.xlabel('Cantidad de Datos (n)', fontsize = 14, fontweight = 'bold')
+
+fig, ax = plt.subplots(1, 1, figsize=(12, 10), sharex=True)
 # Graficar los tiempos de búsqueda promedio
 search_df = avg_df[avg_df['operation'].str.contains('search')]
 for key, grp in search_df.groupby(['hash_type', 'operation']):
-    ax[1].plot(grp['n'], grp['duration'], marker='o', linestyle='-', label=f'{key[0]} - {key[1]}')
+    ax.plot(grp['n'], grp['duration'], marker='o', linestyle='-', label=f'{key}')
 
-ax[1].set_title('Tiempos de Búsqueda Promedio')
-ax[1].set_ylabel('Duración Promedio (ns)')
-ax[1].set_yscale('log')
-#ax[1].set_xscale('log')
-ax[1].legend()
+ax.set_title('Tiempos de Búsqueda Promedio', fontsize = 16, fontweight = 'bold')
+ax.set_ylabel('Duración Promedio (ns)', fontsize = 14, fontweight = 'bold')
+#ax.set_yscale('log')
+#ax.set_xscale('log')
+ax.legend()
 
 # Ajustar la rotación de las etiquetas en el eje x
 plt.xticks(rotation=45)
-plt.xlabel('Cantidad de Datos (n)')
+plt.xlabel('Cantidad de Datos (n)', fontsize = 14, fontweight = 'bold')
+
+# Leer los datos desde el archivo CSV
+df_random = pd.read_csv('results_ids_random.csv', sep=';', names=["hash_type", "operation", "n", "duration"])
+
+# Convertir la columna 'duration' a valores numéricos
+df_random['duration'] = pd.to_numeric(df_random['duration'])
+
+# Calcular el promedio de los tiempos de duración para cada combinación de hash_type, operation y n
+avg_df_random = df_random.groupby(['hash_type', 'operation', 'n']).mean().reset_index()
+
+# Crear subgráficas para inserciones y búsquedas
+fig, ax = plt.subplots(1, 1, figsize=(12, 10), sharex=True)
+
+# Graficar los tiempos de búsqueda promedio
+search_df_random = avg_df_random[avg_df_random['operation'].str.contains('search')]
+for key, grp in search_df_random.groupby(['hash_type', 'operation']):
+    ax.plot(grp['n'], grp['duration'], marker='o', linestyle='-', label=f'{key}')
+
+ax.set_title('Tiempos promedio de Búsqueda con datos que no estan en la tabla', fontsize = 16, fontweight = 'bold')
+ax.set_ylabel('Duración Promedio (ns)', fontsize = 14, fontweight = 'bold')
+#ax.set_yscale('log')
+#ax.set_xscale('log')
+ax.legend()
+
+# Ajustar la rotación de las etiquetas en el eje x
+plt.xticks(rotation=45)
+plt.xlabel('Cantidad de Datos (n)', fontsize = 14, fontweight = 'bold')
 
 # Mostrar la gráfica
 plt.tight_layout()
--- grafic_memory.py
+++ grafic_memory.py
@@ -1,3 +1,4 @@
+from tkinter import font
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -10,26 +11,31 @@ avg_df = df.groupby(['data structure', 'type', 'n']).mean().reset_index()
 ids_df = avg_df[avg_df['data structure'].str.contains('ids')]
 names_df = avg_df[avg_df['data structure'].str.contains('names')]
 
-fig, ax = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
+fig, ax = plt.subplots(1, 1, figsize=(12, 10), sharex=True)
 
-for key, grp in ids_df.groupby(['data structure', 'type']):
-    ax[0].plot(grp['n'], grp['size'], marker='o', linestyle='-', label=f'{key[0]} - {key[1]}')
+for key, grp in ids_df.groupby(['type']):
+    ax.plot(grp['n'], grp['size'], marker='o', linestyle='-', label=f'{key}')
 
-ax[0].set_title('Uso de memoria promedio(ids)')
-ax[0].set_ylabel('Tamaño (Kilobytes)')
+ax.set_title('Uso de memoria promedio(ids)', fontsize = 16, fontweight = 'bold')
+ax.set_ylabel('Tamaño (Kilobytes)', fontsize = 14, fontweight = 'bold')
 #ax.set_yscale('log')
-ax[0].legend()
-
-for key, grp in names_df.groupby(['data structure', 'type']):
-    ax[1].plot(grp['n'], grp['size'], marker='o', linestyle='-', label=f'{key[0]} - {key[1]}')
-
-ax[1].set_title('Uso de memoria promedio(usernames)')
-ax[1].set_ylabel('Tamaño (Kilobytes)')
-#ax.set_yscale('log')
-ax[1].legend()
+ax.legend()
 
 plt.xticks(rotation=45)
-plt.xlabel('Cantidad de Datos (n)')
+plt.xlabel('Cantidad de Datos (n)', fontsize = 14, fontweight = 'bold')
+
+fig, ax1 = plt.subplots(1, 1, figsize=(12, 10), sharex=True)
+
+for key, grp in names_df.groupby(['type']):
+    ax1.plot(grp['n'], grp['size'], marker='o', linestyle='-', label=f'{key}')
+
+ax1.set_title('Uso de memoria promedio(usernames)', fontsize = 16, fontweight = 'bold')
+ax1.set_ylabel('Tamaño (Kilobytes)', fontsize = 14, fontweight = 'bold')
+#ax1.set_yscale('log')
+ax1.legend()
+
+plt.xticks(rotation=45)
+plt.xlabel('Cantidad de Datos (n)', fontsize = 14, fontweight = 'bold')
 
 plt.tight_layout()
 plt.show()
